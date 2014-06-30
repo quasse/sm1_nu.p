21f58a5f955c6398b09c0c9354858a6cd5e9fa86
Sam Morgen <quasse@gmail.com>
2014-06-30 09:24:30 -0500

    Set up the Main method

---
+++ nu.p/src/nu/p/Main.java
@@ -0,0 +1,18 @@
+package nu.p;
+
+import java.util.stream.Stream;
+
+import org.apache.log4j.*;
+
+public class Main {
+
+	private static Logger logger = Logger.getLogger(Stream.class);
+	
+	public static void main(String[] args) {
+		
+		//Initialize the Logger
+		BasicConfigurator.configure();
+
+	}
+
+}
