eda71a488033248f25c0259c91346ba7a2b4a9da
Sam Morgen <quasse@gmail.com>
2014-07-08 13:29:08 -0500

    Put hyperlink in frame

--- nu.p/src/nu/p/Frame.java
+++ nu.p/src/nu/p/Frame.java
@@ -2,13 +2,19 @@ package nu.p;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.GridBagLayout;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 
+import javax.swing.JButton;
 import javax.swing.JEditorPane;
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
 import javax.swing.event.HyperlinkEvent;
 import javax.swing.event.HyperlinkListener;
 
@@ -21,31 +27,46 @@ public class Frame {
 	private final String FRAME_NAME = "News Stream";
 	private Stream stream = new Stream();
 	private Logger logger;
-	
-	public Frame(Logger logger){
 
-		//initializes logger
-		this.logger = logger;
-		
-		
-		//Creates the frame
-		frame = new JFrame(FRAME_NAME);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(FRAME_DIMENSION);
-		frame.getContentPane().setBackground(Color.black);
-		
-		//Sets up EditorPane
-		JEditorPane editor = new JEditorPane();
-		editor.setEditable(false);
-		editor.setForeground(Color.GREEN);
-		editor.setBackground(Color.BLACK);
+	public Frame(Logger logger) throws URISyntaxException{
+
 
 		//Gets the first Reddit link
 		String elm = stream.getNextRedditElement();
 		String[] parts = elm.split("-");
 		String headline = parts[0];
 		String link = parts[1];
-		
+
+		final URI uri = new URI("http://java.sun.com");
+
+		//initializes logger
+		this.logger = logger;
+
+		//Creates the frame
+		frame = new JFrame(FRAME_NAME);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(FRAME_DIMENSION);
+		Container container = frame.getContentPane();
+
+		container.setLayout(new GridBagLayout());
+		JButton button = new JButton();
+		button.setText("<HTML>Click the <FONT color=\"#000099\"><U>link</U></FONT>"
+				+ " to go to the Java website.</HTML>");
+		button.setHorizontalAlignment(SwingConstants.LEFT);
+		button.setBorderPainted(false);
+		button.setOpaque(false);
+		button.setBackground(Color.WHITE);
+		button.setToolTipText(uri.toString());
+		button.addActionListener(new OpenUrlAction(uri));
+		container.add(button);
+		/**
+		//Sets up EditorPane
+		JEditorPane editor = new JEditorPane();
+		editor.setEditable(false);
+		editor.setForeground(Color.GREEN);
+		editor.setBackground(Color.BLACK);
+
+
 		//Sets up a text area 
 		JTextArea area = new JTextArea();
 		area.setEditable(false);
@@ -53,23 +74,11 @@ public class Frame {
 		area.setForeground(Color.GREEN);
 		area.setBackground(Color.BLACK);
 		area.append(headline);
-		
-		
-		
-		//Sets up hyperlink
-		URL url = null;
-		try {
-			url = new URL(link);
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		}
-		HyperlinkEvent event = new HyperlinkEvent(area, 
-				HyperlinkEvent.EventType.ENTERED, url);
 
-		frame.getContentPane().add(area, BorderLayout.PAGE_START);
+		frame.getContentPane().add(area, BorderLayout.PAGE_START);**/
 		frame.setVisible(true);
 	}
-	
+
 	public String nextElements(){
 		return stream.getNextRedditElement();
 	}
--- nu.p/src/nu/p/Main.java
+++ nu.p/src/nu/p/Main.java
@@ -1,12 +1,14 @@
 package nu.p;
 
+import java.net.URISyntaxException;
+
 import org.apache.log4j.*;
 
 public class Main {
 
 	private static Logger logger = Logger.getLogger(Stream.class);
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws URISyntaxException {
 		
 		//Initialize the Logger
 		BasicConfigurator.configure();
